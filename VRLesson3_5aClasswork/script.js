63bc032a3d7532ab7fb85e3a9c3bd49d8c13a0cc
yunzhaoz <yunzhaoz@nycstudents.net>
2025-12-01 21:23:54 +0000

    12/1

--- VRLesson3_5aClasswork/script.js
+++ VRLesson3_5aClasswork/script.js
@@ -6,9 +6,11 @@ window.addEventListener("DOMContentLoaded",function() {
   camera = document.querySelector("a-camera");
   target = document.querySelector("#target");
   // Challenge 2 - Create a variable for the second target
+  target2 = document.querySelector("#target2");
   ball = document.querySelector("#ball");
   ball.x = -1;
   ball.dx = -0.002;
+  
   
   setTimeout(loop,100);
 })
@@ -19,14 +21,14 @@ function loop(){
   ball.object3D.position.x = ball.x
   let d1 = distance(ball, target);
   //Challenge 3 - Store the distance between the ball and the second target
-
+  let d2 = distance(ball, target2);
   output.setAttribute("value",`d1= ${d1}\nd2=${d2}`);
 
   /*  Challenge 4
       Modify the decision below so that the direction of the ball is reversed 
       when the distance between target 1 OR target 2 is less than 1
   */
-  if(d1 < 1){
+  if(d1 < 1 || d2 < 1){
     ball.dx = -ball.dx;
   }
 
